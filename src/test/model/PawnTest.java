b626ad1ab55b79bdc5e5ae9830ab35907c9d3c99
Kent <kc1225@students.cs.ubc.ca>
2021-11-19 12:22:23 -0800

    - increase readability of code - increase code coverage

--- src/test/model/PawnTest.java
+++ src/test/model/PawnTest.java
@@ -70,25 +70,25 @@ public class PawnTest {
 
     @Test
     public void testPossibleMovesNoBlockHasMoved() {
-        placeOnBoard(pawn, 6, 7);
+        placeOnBoard(pawn, 8, 7);
         placeOnBoard(king, 6, 8);
         white.add(pawn);
         white.add(king);
         setGame();
-        game.move(pawn, 6, 5);
+        game.move(pawn, 8, 5);
         ArrayList<Position> pm = pawn.possibleMoves(game);
         ArrayList<Position> expected = new ArrayList<>();
-        expected.add(new Position(6, 4));
+        expected.add(new Position(8, 4));
         assertEquals(expected.size(), pm.size());
         arrayListEquals(pm, expected);
     }
 
     @Test
     public void testPossibleMovesBlockByTeammate() {
-        placeOnBoard(pawn, 6, 7);
+        placeOnBoard(pawn, 1, 7);
         placeOnBoard(king, 6, 8);
         Bishop bishop = new Bishop("white");
-        placeOnBoard(bishop, 6, 6);
+        placeOnBoard(bishop, 1, 6);
         white.add(pawn);
         white.add(king);
         white.add(bishop);
@@ -117,13 +117,35 @@ public class PawnTest {
 
     @Test
     public void testPossibleMovesNoSpaceInFront() {
+        Pawn pawn1 = new Pawn("white");
+        Pawn pawn2 = new Pawn("white");
+        Pawn pawn3 = new Pawn("black");
+        Pawn pawn4 = new Pawn("black");
+        Pawn pawn5 = new Pawn("black");
+        King blackKing = new King("black");
         placeOnBoard(pawn, 6, 1);
-        placeOnBoard(king, 6, 8);
+        placeOnBoard(pawn1, 1, 1);
+        placeOnBoard(pawn2, 8, 1);
+        placeOnBoard(pawn3, 6, 8);
+        placeOnBoard(pawn4, 1, 8);
+        placeOnBoard(pawn5, 8, 8);
+        placeOnBoard(king, 8, 5);
+        placeOnBoard(blackKing, 1,5);
         white.add(pawn);
+        white.add(pawn1);
+        white.add(pawn2);
         white.add(king);
+        black.add(pawn3);
+        black.add(pawn4);
+        black.add(pawn5);
+        black.add(blackKing);
         setGame();
-        ArrayList<Position> pm = pawn.possibleMoves(game);
-        assertEquals(0, pm.size());
+        assertEquals(0, pawn.possibleMoves(game).size());
+        assertEquals(0, pawn1.possibleMoves(game).size());
+        assertEquals(0, pawn2.possibleMoves(game).size());
+        assertEquals(0, pawn3.possibleMoves(game).size());
+        assertEquals(0, pawn4.possibleMoves(game).size());
+        assertEquals(0, pawn5.possibleMoves(game).size());
     }
 
     @Test
